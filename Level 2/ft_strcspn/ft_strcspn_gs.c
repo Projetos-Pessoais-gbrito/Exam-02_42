8d9b773be5b9ee0cc547ab90232770ba5728c6d2
GustavonBrito <gustavoserafio@gmail.com>
2025-01-15 23:35:55 -0300

    feat: implementing strcspn

--- Level 2/ft_strcspn/ft_strcspn_gs.c
+++ Level 2/ft_strcspn/ft_strcspn_gs.c
@@ -6,7 +6,43 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 02:30:01 by codespace         #+#    #+#             */
-/*   Updated: 2025/01/15 22:45:10 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/01/15 23:28:06 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+#include <stdio.h>
+
+size_t	ft_strcspn(const char *s, const char *reject)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while(s[i])
+	{
+		while(reject[j])
+		{
+			if (s[i + j] == reject[j])
+			{
+				j++;
+			}
+			else
+			{
+				j = 0;
+				break;
+			}
+		}
+		if (reject[j] == '\0')
+			break;
+		i++;
+	}
+	printf("%d\n", j);
+}
+
+int main ()
+{
+	ft_strcspn("gustavo", "a");
+	printf("%ld", strcspn("gustavo", "a"));
+}
